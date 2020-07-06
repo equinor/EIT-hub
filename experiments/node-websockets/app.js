6b9cc1842aaff0e0f364ee6f818ed4236dae7fe9
michoy <michaelhoyer@outlook.com>
2020-07-06 11:35:08 +0200

    add winston logger

--- experiments/node-websockets/app.js
+++ experiments/node-websockets/app.js
@@ -1,9 +1,37 @@
-var WebSocketServer = require('websocket').server;
-var http = require('http');
+const WebSocketServer = require('websocket').server;
+const http = require('http');
+const winston = require('winston')
 
-var server = http.createServer(function(request, response) {
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  defaultMeta: { service: 'user-service' },
+  transports: [
+    //
+    // - Write all logs with level `error` and below to `error.log`
+    // - Write all logs with level `info` and below to `combined.log`
+    //
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'combined.log' }),
+  ],
+});
+
+//
+// If we're not in production then log to the `console` with the format:
+// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
+//
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(new winston.transports.Console({
+    format: winston.format.simple(),
+  }));
+}
+
+
+const server = http.createServer(function(request, response) {
   // process HTTP request. Since we're writing just WebSockets
   // server we don't have to implement anything.
+  winston.log('info', 'A client has connected to the server')
 });
 server.listen(1337, function() { });
 
@@ -16,17 +44,20 @@ wsServer = new WebSocketServer({
 wsServer.on('request', function(request) {
   var connection = request.accept(null, request.origin);
 
-  connection.sendUTF("Client has connected to server");
+  connection.sendUTF("Client has connected to websocket");
+  winston.log('info', 'A client has connected to the websocket')
 
   // This is the most important callback for us, we'll handle
   // all messages from users here.
   connection.on('message', function(message) {
     if (message.type === 'utf8') {
+      winston.log('info', 'Message recieved: ', message)
       connection.sendUTF(message);
     }
   });
 
   connection.on('close', function(connection) {
     // close user connection
+    winston.log('info', 'A client has disconnected')
   });
 });
