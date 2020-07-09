0e4ffffc600c7de90d19508a107d7219df34aca0
Ole Martin Gjersvik <olgj@equinor.com>
2020-07-09 16:00:59 +0200

    The rest of Shuttle part of the server.

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,13 @@
 "use strict";
 
 const Auth = require('./auth');
+const AzureIot = require('./azureiot');
 const BrowserWs = require('./browser-ws');
 const config = require('./config');
 const DeviceWS = require('./device-ws');
 const Express = require('./express');
+const ShuttleControl = require('./shuttle-control');
+const ShuttleTelemetry = require('./shuttle-telemetry');
 
 /** The main function for Eit-Hub backend.
  *  We should not have js code directly in the file.
@@ -23,11 +26,23 @@ function main() {
     //Setup Device Websocket
     const deviceWs = new DeviceWS();
 
+    //Setup Azure IoT
+    const azureIot = new AzureIot(configObj);
+
+    //Setup ShuttleControl
+    const shuttleControl = new ShuttleControl(azureIot, browserWs, deviceWs);
+
+    //Setup ShuttleTelemetry
+    const shuttleTelemetry = new ShuttleTelemetry(azureIot, browserWs, deviceWs);
+
     //Create express
     const express = new Express(configObj.port, auth, browserWs, deviceWs)
 
-    // Starts server
+    // Start EitHub
+    shuttleControl.start();
+    shuttleTelemetry.start();
     express.start();
+
 }
 
 // Invoke main and starts the server
---
+++ backend/shuttle-control.js
@@ -0,0 +1,11 @@
+class ShuttleControl {
+    constructor(azureIot, browserWs, deviceWs) {
+
+    }
+
+    start(){
+        
+    }
+}
+
+module.exports = ShuttleControl;
