f87e91c204184858434b6abac67d4b66b9fed7be
prinsWindy <markus.iversflaten@hotmail.com>
2020-07-13 15:22:54 +0200

    Implement 'sendMessage'

--- backend/azureiot.js
+++ backend/azureiot.js
@@ -4,9 +4,16 @@
  * 
  * This is the only part of the code that can use Azure IoT apis. Add method as needed.
  */
+
+const Client = require('azure-iothub').Client;
+const Message = require('azure-iot-common').Message;
+const config = require('./config');
+
 class AzureIot {
     constructor(config) {
-
+        this.configObj = config.main();
+        this.connectionString = configObj.IOTHUB_CONNECTION_STRING;
+        this.client = Client.fromConnectionString(this.connectionString);
     }
 
     /** Try to send a message to a device. If the device do not exist then it will be dropped.
@@ -16,11 +23,16 @@ class AzureIot {
      * @returns {boolean} If there was nowhere to send to.
      */
     sendMessage(deviceName, jsonObject) {
-        //TODO needs to handle a connection at some point.
-
-        console.log(`Trying to send message to ${deviceName} but its not implemented. Payload:\n`, jsonObject);
-        
-        return false;
+        var message = new Message(jsonObject);
+        message.messageId = 'c2d';
+        console.log(`Sending message to ${deviceName} with payload:\n`, message.getData())
+        this.client.send(deviceName, message, function(err) {
+            if (err) {
+                return false;
+            } else {
+                return true;
+            }
+        });
     }
 
     /** Register a callback for used with device.
