3f89c165de801648ee5b63f4f20cc1a1d36fc1ea
prinsWindy <markus.iversflaten@hotmail.com>
2020-07-14 15:48:32 +0200

    Implement real-time shuttle telemetry

--- backend/shuttle-telemetry.js
+++ backend/shuttle-telemetry.js
@@ -1,10 +1,26 @@
 class ShuttleTelemetry {
     constructor(azureIot, browserWs, deviceWs) {
-
+        this.azureIot = azureIot;
+        this.browserWs = browserWs;
+        this.deviceWs = deviceWs;
     }
 
     start(){
-        
+        let self = this;
+
+        // Receive and forward real time telemetry from shuttle
+        self.deviceWs.onMessage('shuttle', function(message)  {
+            if (message.type === 'telemetry') {
+                self.browserWs.broadcast(message);
+            }
+            // TODO: Validate message when telemetry format has been chosen
+            console.warn(`Message from device has unkown type: ${message.type}`);
+        });
+
+        // Receive and forward aggregated telemetry from shuttle
+        self.azureIot.onMessage('shuttle', function(message)  {
+            // TODO
+        });
     }
 }
 
