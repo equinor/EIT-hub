d9c7e4aad1f55f6e3d0449b1cfc10b9b459d4080
prinsWindy <markus.iversflaten@hotmail.com>
2020-07-14 14:24:30 +0200

    Implement ShuttleControl

--- backend/shuttle-control.js
+++ backend/shuttle-control.js
@@ -1,10 +1,33 @@
 class ShuttleControl {
     constructor(azureIot, browserWs, deviceWs) {
-
+        this.azureIot = azureIot;
+        this.browserWs = browserWs;
+        this.deviceWs = deviceWs;
+        this.currentBrowser = 0;
     }
 
     start(){
-        
+        let self = this;
+
+        // Listens for all websocket messages with type 'input'
+        self.browserWs.onTopic('input', function(message)  {
+            let browserId = message.browserId;
+            if (browserId >= self.currentBrowser) {
+                self.currentBrowser = browserId;
+                let input = message.body;
+                
+                // Check that body contains only x, y, z, r
+                const neededKeys = ['x', 'y', 'z', 'r'];
+                if (Object.keys(input).every(key => neededKeys.includes(key))) {
+                    self.deviceWs.sendMessage('shuttle', input);
+                } else {
+                    console.error('JSON object contains invalid data.');
+                    return;
+                }
+            } else {
+                return;
+            }
+        });
     }
 }
 
