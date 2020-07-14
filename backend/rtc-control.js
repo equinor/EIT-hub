05b8512d1f13dafc64cbdb044bb0c3b6c6c0fbd8
Ole Martin Gjersvik <olgj@equinor.com>
2020-07-14 13:48:14 +0200

    First try on a srtucture for video part of EITHub

---
+++ backend/rtc-control.js
@@ -0,0 +1,14 @@
+/**
+ * Managers many VideoStreams. And link it up with azure iot and connection with browser.
+ */
+class RtcControl {
+    constructor(azureIot, browserWS) {
+        //TODO
+    }
+
+    start() {
+        //TODO
+    }
+}
+
+module.exports = RtcControl;
