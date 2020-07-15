e066432ea93b1077850b44b9b41fe7bf538d28a2
Asmami <Asbjorn.midtbo@gmail.com>
2020-07-15 16:12:23 +0200

    First draft of video-stream

--- backend/video-stream.js
+++ backend/video-stream.js
@@ -3,12 +3,103 @@
  * 
  * This is the only class that can talk to SimplePeer directly.
  */
+
+const SimplePeer = require("simple-peer");
+const wrtc = require("wrtc");
+
+
 class VideoStream {
-    constructor () {
-        //TODO
+
+    /** Creates device peer connection on offer. Returns SDP answer, peer and stream.
+     * 
+     * @param {string} DeviceSDP
+     * @returns {any} json object that contains SDP answer, peer and stream 
+     */
+
+    createDevicePeer(DeviceSDP) {
+
+        var video;
+        var sdp;
+        var returnObj;
+        
+        var DevicePeer = new SimplePeer({
+            trickle: false,
+            wrtc: wrtc
+        }); 
+
+        DevicePeer.on('error', err => console.log('error', err));
+        DevicePeer.on('stream', stream => { video = stream });
+
+        DevicePeer.on('signal', data => {
+            console.log("Generated SDP answer for device");
+            sdp = JSON.stringify(data);
+        });
+
+        DevicePeer.signal(JSON.parse(DeviceSDP));
+        console.log("Submited device SDP");
+
+        returnObj = { stream: video, peer: DevicePeer, sdp: sdp };
+
+        return returnObj;
+        
     }
 
-    //TODO find a goodish api.
+
+    /** Creates client peer on browser request. Returns SDP offer and peer.  
+     * 
+     * @param {any} Stream
+     * @returns {any} json object that contains SDP offer and peer
+     */
+
+    createClientPeer(Stream) {
+
+        var sdp;
+        var returnObj;
+
+        ClientPeer = new SimplePeer({
+            initiator: true,
+            stream: Stream,
+            trickle: false,
+            wrtc: wrtc
+        });
+
+        ClientPeer.on('error', err => console.log('error', err));
+
+        ClientPeer.on('signal', data => {
+            console.log("Generated client SDP");
+            sdp = JSON.stringify(data); 
+        });
+
+        returnObj = { peer: ClientPeer, sdp: sdp };
+
+        return returnObj;
+        
+    }
+
+
+    /** Submits client SDP. 
+     * 
+     * @param {any} ClientPeer
+     * @param {string} ClientSDP 
+     */
+
+    submitClientSDP(ClientPeer, ClientSDP) {
+        ClientPeer.signal(JSON.parse(ClientSDP));
+        console.log("Submited client SDP");
+    }
+
+
+    /** Destroys selected peer conection.
+     * 
+     * @param {any} Peer 
+     */
+
+    PeerDestroy(Peer){
+        Peer.destroy();
+        console.log("Peer connection destroyed");
+    }
+
+
 }
 
-module.exports = VideoStream;
+module.exports = VideoStream; 
