22d92f108ff4c2e8726a07528fa25c9243f17d71
prinsWindy <markus.iversflaten@hotmail.com>
2020-07-17 16:51:08 +0200

    Add fallback for connection strings

--- backend/azureiot.js
+++ backend/azureiot.js
@@ -11,9 +11,14 @@ const { EventHubConsumerClient } = require("@azure/event-hubs");
 
 class AzureIot {
     constructor(config) {
+        this.config = config;
         this._onMessageCallbacks = new Map();
-        this.iotHubClient = Client.fromConnectionString(config.iotHubConnectionString);
-        this.eventHubConsumer = new EventHubConsumerClient("$Default", config.eventHubConnectionString);
+        if (this.config.iotHubConnectionString !== "") {
+            this.iotHubClient = Client.fromConnectionString(this.config.iotHubConnectionString);
+        }
+        if (this.config.eventHubConnectionString !== "") {
+            this.eventHubConsumer = new EventHubConsumerClient("$Default", this.config.eventHubConnectionString);
+        }
     }
 
     /** Try to send a message to a device. If the device do not exist then it will be dropped.
@@ -23,16 +28,23 @@ class AzureIot {
      * @returns {boolean} If there was nowhere to send to.
      */
     sendMessage(deviceName, jsonObject) {
-        var message = new Message(JSON.stringify(jsonObject));
-        message.messageId = 'c2d';
-        console.log(`Sending message to ${deviceName} with payload:\n`, message.getData())
-        this.client.send(deviceName, message, function(err) {
-            if (err) {
-                return false;
-            } else {
-                return true;
-            }
-        });
+        let self = this;
+
+        if (self.config.iotHubConnectionString !== "") {
+
+            let message = new Message(JSON.stringify(jsonObject));
+            message.messageId = 'c2d';
+            console.log(`Sending message to ${deviceName} with payload:\n`, message.getData())
+            this.client.send(deviceName, message, function (err) {
+                if (err) {
+                    return false;
+                } else {
+                    return true;
+                }
+            });
+        } else {
+            console.warn('You have not specified an IoT Hub connection string.')
+        }
     }
 
     /** Register a callback for used with device.
@@ -41,39 +53,44 @@ class AzureIot {
      * @param {Function} callback called with a js object with the message from device as argument.
      */
     onMessage(deviceName, callback) {
-        if (this._onMessageCallbacks.has(deviceName)){
+        if (this._onMessageCallbacks.has(deviceName)) {
             this._onMessageCallbacks.get(deviceName).push(callback);
         } else {
             let emptyArr = [callback];
-            this._onMessageCallbacks.set(deviceName,emptyArr);
+            this._onMessageCallbacks.set(deviceName, emptyArr);
         }
     }
 
     async start() {
         let self = this;
 
-        let _processMessage = function(messages) {
-            for (const message of messages) {
-                let deviceName = message.systemProperties['iothub-connection-device-id'];
+        if (self.config.eventHubConnectionString !== "") {
 
-                if (self._onMessageCallbacks.has(deviceName) && self._onMessageCallbacks.get(deviceName).length > 0) {
-                    for (let callback of self._onMessageCallbacks.get(deviceName)) {
-                        callback(message);
+            let _processMessage = function (messages) {
+                for (const message of messages) {
+                    let deviceName = message.systemProperties['iothub-connection-device-id'];
+
+                    if (self._onMessageCallbacks.has(deviceName) && self._onMessageCallbacks.get(deviceName).length > 0) {
+                        for (let callback of self._onMessageCallbacks.get(deviceName)) {
+                            callback(message);
+                        }
                     }
                 }
-            }
-        };
+            };
 
-        let _processError = function(err) {
-            console.log(err);
-        };
-      
-        // Subscribe to messages from all partitions as below
-        self.eventHubConsumer.subscribe({
-          processEvents: _processMessage,
-          processError: _processError
-        });
-      }
+            let _processError = function (err) {
+                console.log(err);
+            };
+
+            // Subscribe to messages from all partitions as below
+            self.eventHubConsumer.subscribe({
+                processEvents: _processMessage,
+                processError: _processError
+            });
+        } else {
+            console.warn('You have not specified an Event Hubs connection string.')
+        }
+    }
 }
 
 module.exports = AzureIot;
