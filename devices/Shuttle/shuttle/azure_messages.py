dd0d7af82514dd514dd55a996250ec97229e8e74
prinsWindy <markus.iversflaten@hotmail.com>
2020-07-23 14:40:44 +0200

    Implement azure message listener

---
+++ devices/Shuttle/shuttle/azure_messages.py
@@ -0,0 +1,15 @@
+from azure.iot.device import IoTHubDeviceClient
+import json
+
+class AzureMessageListener:
+
+    def __init__(self, connection_string):
+        self.connection_string = connection_string
+        self.device_client = IoTHubDeviceClient.create_from_connection_string(self.connection_string)
+
+        self.device_client.connect()
+
+    def message_listener(self):
+        message = self.device_client.receive_message()  # blocking call
+        jsonMsg = json.loads(message.data)
+        return jsonMsg['url'], jsonMsg['token']
