d667f9a00074c6fdb2b24740f667baa84c41f735
prinsWindy <markus.iversflaten@hotmail.com>
2020-07-24 11:05:11 +0200

    Update docstring

--- public/gamepad.js
+++ public/gamepad.js
@@ -2,11 +2,11 @@ export default class GamePad {
 
     /** Gets the axis of the first gamepad connected 
      * 
-     * @returns {[number]} An array of values between -1 and 1 for etch axis supposed by the controller.
+     * @returns The axes and buttons available for the connected gamepad controller.
+     * These are returned in two separate arrays.
+     * If no gamepad is detected, return null.
      */
     getGamepad() {
-        //TODO need to handle the case where there is no gamepad connected. This function should return something useful like an axes of only 0.0
-
         let gamepads = navigator.getGamepads ? navigator.getGamepads() : (navigator.webkitGetGamepads ? navigator.webkitGetGamepads : []);
         if (gamepads[0] !== null)
             return [gamepads[0].axes, gamepads[0].buttons];
