285760724864f77c54a6f5d5464866801164c902
Ole Martin Gjersvik <olgj@equinor.com>
2020-09-04 10:26:16 +0200

    Shuttle ui is now part of new structure.

--- apps/shuttle/public/index.ts
+++ apps/shuttle/public/index.ts
@@ -5,13 +5,19 @@ import InputView from './inputView';
 import Input from './input';
 import TelemetryView from './telemetryView';
 import ClientRequestView from './clientRequests';
+import html from './index.html';
 
-export default function main(): void {
+export default function main(): HTMLElement {
+    // Parse Dom
+    const parser = new DOMParser();
+
+    const doc = parser.parseFromString(html, "text/html");
+
     // Setup websocket.
     const websocket = new WebSocket();
 
     // Setup Telemetry
-    const telemetryView = new TelemetryView(document.getElementById('telemetry')!);
+    const telemetryView = new TelemetryView(doc.getElementById('telemetry')!);
     websocket.onTelemetry(telemetryView.updateTelemetry.bind(telemetryView));
 
     // Setup GamePad.
@@ -21,15 +27,17 @@ export default function main(): void {
     const keyboard = new Keyboard(window);
 
     // Setup InputView.
-    const inputView = new InputView(document.getElementById('input-view')!);
+    const inputView = new InputView(doc.getElementById('input-view')!);
 
     // Setup Input
     const input = new Input(websocket,gamePad,keyboard,inputView);
 
     // Setup ClientRequests
-    const clientRequestView = new ClientRequestView(websocket,document.getElementById('clientRequests')!)
+    const clientRequestView = new ClientRequestView(websocket,doc.getElementById('clientRequests')!)
     websocket.onControl(clientRequestView.updateControl.bind(clientRequestView));
 
     // Start client
     input.start(200);
+
+    return doc.body.firstChild as HTMLElement;
 }
--- core/client/index.ts
+++ core/client/index.ts
@@ -1,6 +1,8 @@
-import shuttle_html from "../../apps/shuttle/public/index.html";
 import shuttle from "../../apps/shuttle/public/index";
 
-document.body.innerHTML = shuttle_html;
+function main() {
+    const eitMain = document.getElementsByTagName("eit-main")[0];
+    eitMain.appendChild(shuttle());
+}
 
-shuttle();
+main();
