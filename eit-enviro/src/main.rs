826f66e8f7eb587f8d53a8ef845a6c52536f492b
Ole Martin Gjersvik <olgj@equinor.com>
2020-10-16 09:09:11 +0200

    Added mqtt clinet.

--- eit-enviro/src/main.rs
+++ eit-enviro/src/main.rs
@@ -1,8 +1,44 @@
 extern crate linux_embedded_hal as hal;
 extern crate ltr_559;
+use rumqttc::QoS;
+use std::sync::Arc;
+use rumqttc::MqttOptions;
+use rumqttc::Client;
+use rumqttc::ClientConfig;
 use ltr_559::{AlsGain, AlsIntTime, AlsMeasRate, Ltr559, SlaveAddr};
+use std::env;
+use serde::{Serialize, Deserialize};
+
+
 
 fn main() {
+    let args:Vec<String> = env::args().collect();
+    let mut sensor = setup_sensor();
+    let (mut client, mut connection) = setup_mqtt(&args[1]);
+
+    std::thread::spawn(move || {
+        // we are not handling incoming requests
+        for _ in connection.iter() {}
+    });
+
+    let mut enviro = Enviro{lux: 0.0};
+    
+    loop {
+        let status = sensor.get_status().unwrap();
+        if status.als_data_valid {
+            enviro.lux = sensor.get_lux().unwrap();
+            let _ = client.publish("public/eithub/enviro", QoS::AtMostOnce, false, serde_json::to_vec(&enviro).unwrap());
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Enviro {
+    lux: f32,
+}
+
+
+fn setup_sensor() -> ltr_559::Ltr559<hal::I2cdev, ltr_559::ic::Ltr559> {
     let dev = hal::I2cdev::new("/dev/i2c-1").unwrap();
     let address = SlaveAddr::default();
     let mut sensor = Ltr559::new_device(dev, address);
@@ -13,19 +49,17 @@ fn main() {
 
     sensor.set_als_meas_rate(AlsIntTime::_50ms, AlsMeasRate::_50ms).unwrap();
     sensor.set_als_contr(AlsGain::Gain4x, false, true).unwrap();
-    
-    loop {
-        let status = sensor.get_status().unwrap();
-        if status.als_data_valid {
-            let lux = sensor.get_lux().unwrap();
-            println!("Lux = {}" , lux);
-        }
-        if status.ps_data_status {
-            let (proximity, saturation) = sensor.get_ps_data().unwrap();
-            println!(
-                "Proximity = {}, Saturation = {}",
-                proximity, saturation
-            );
-        }
-    }
+    sensor
+}
+
+fn setup_mqtt(passwd: &str)-> (rumqttc::Client, rumqttc::Connection) {
+    let mut config = ClientConfig::new();
+    config.root_store.add_server_trust_anchors(&webpki_roots::TLS_SERVER_ROOTS);
+
+    let mut mqtt_options = MqttOptions::new("eit-enviro", "eit-hub-broker.northeurope.cloudapp.azure.com", 8883);
+    mqtt_options.set_keep_alive(5);
+    mqtt_options.set_credentials("device", passwd);
+    mqtt_options.set_tls_client_config(Arc::new(config));
+
+    Client::new(mqtt_options, 20)
 }
